2c90d7f6c43e14e31ae040d552617c6dad7539d3
gm2870 <alifarzanegan70@gmail.com>
2023-10-20 01:05:39 +0330

    created card component

---
+++ components/ui/Card/Card.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+const Card = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="bg-white shadow-[0_0_1rem_rgba(0,0,0,0.122)] rounded-xl p-3">
+      {children}
+    </div>
+  );
+};
+
+export default Card;
